8cecd17564acc898050653b1caa72882dcf37aa3
Stephen R. Foster <stephen@thoughtstem.com>
2020-11-06 23:26:56 +0000

    first commit

---
+++ main.rkt
@@ -0,0 +1,38 @@
+#lang codespells
+
+(require ice-particles/mod-info)
+
+(define-classic-rune (ice-storm)
+  #:background "blue"
+  #:foreground (circle 40 'solid 'green)
+  (spawn-mod-blueprint pak-folder mod-name "IceStorm"))
+
+(define-classic-rune (ice-ball)
+  #:background "blue"
+  #:foreground (circle 40 'solid 'red)
+  (spawn-mod-blueprint pak-folder mod-name "IceBall"))
+
+(define-classic-rune (ice-ball-hit)
+  #:background "blue"
+  #:foreground (circle 40 'solid 'yellow)
+  (spawn-mod-blueprint pak-folder mod-name "IceBallHit"))
+
+(define-classic-rune (ice-aura)
+  #:background "blue"
+  #:foreground (circle 40 'solid 'blue)
+  (spawn-mod-blueprint pak-folder mod-name "IceAura"))
+
+(define-classic-rune-lang my-mod-lang #:eval-from main.rkt
+  (ice-storm ice-ball ice-ball-hit ice-aura)
+  )
+
+(module+ main
+  (codespells-workspace ;TODO: Change this to your local workspace if different
+   (build-path (current-directory) ".." ".."))
+  
+  (once-upon-a-time
+   #:world (voxel-world)
+   #:aether (demo-aether
+             #:lang (my-mod-lang #:with-paren-runes? #t))))
+
+
---
+++ scribblings/ice-particles.scrbl
@@ -0,0 +1,10 @@
+#lang scribble/manual
+@require[@for-label[ice-particles
+                    racket/base]]
+
+@title{ice-particles}
+@author{Administrator}
+
+@defmodule[ice-particles]
+
+Package Description Here
