49f556ac76121191b53d6dbf2bbe5caa1dad7703
srfoster <lindseyhandley@gmail.com>
2020-11-10 14:05:29 -0800

    reorg

---
+++ lore.rkt
@@ -0,0 +1,48 @@
+#lang at-exp racket
+
+(require ice-particles/main
+	 website-js)
+
+(provide name
+         description
+         runes
+         preview-image
+         )
+
+(define (name)
+  "Ice Particles"
+  )
+
+(define (description)
+  @md{
+    This is the @b{Ice Particles} Rune Collection.
+  })
+
+(define (runes)
+  (list
+    (list
+     "Ice Storm"
+     (ice-storm-rune)
+     "" 
+     )
+    (list
+     "Ice Ball Rune"
+     (ice-ball-rune)
+     "" 
+     )
+    (list
+     "Ice Ball Hit Rune"
+     (ice-ball-hit-rune)
+     "" 
+     )
+    (list
+     "Ice Aura Rune"
+     (ice-aura-rune)
+     "" 
+     )
+    )
+  )
+
+(define (preview-image)
+  (ice-storm-rune)
+  )
