fe05bc4d3ffbbff43e99767e402291b57f98721b
srfoster <lindseyhandley@gmail.com>
2020-11-13 11:45:42 -0800

    fixing lindseys name

--- lore.rkt
+++ lore.rkt
@@ -13,7 +13,7 @@
   @md{
   The @b{Illusions of Ice} Rune Collection was crafted by @b{The Snow Mages of Kafdor.} The Runes have been passed down from generation to generation and play a key role in coming of age ceremonies in this culture.
 
-  I, @b{Lindsey the Arch Wizard Scientist}, have been studying Kafdor culture for several years and documenting their Runes. I will document new Runes as they come to light.
+  I -- @b{Lindsey the Arch Wizard Scientist} -- have been studying Kafdor culture for several years and documenting their Runes. I will document new Runes as they come to light.
   }
   #:rune-lores
 
