9e0c0dc436f483495f2cbfaa978c7aec01504aaa
Stephen Foster <stephen@thoughtstem.com>
2020-11-13 11:06:17 -0800

    videos and lore

--- lore.rkt
+++ lore.rkt
@@ -4,38 +4,68 @@
 	 codespells/lore)
 
 (define-runtime-path preview-image
-                     "images/preview.png"
-                     )
+                     "images/preview.png")
+
 
 (define-rune-collection-lore 
   #:name "Illusions of Ice"
   #:description 
   @md{
-  The @b{Illusions of Ice} Rune Collection was crafted by @b{The Snow Mages} of Kafdor. The Runes have been passed down from generation to generation and play a key role in coming of age ceremonies in this culture.
+  The @b{Illusions of Ice} Rune Collection was crafted by @b{The Snow Mages of Kafdor.} The Runes have been passed down from generation to generation and play a key role in coming of age ceremonies in this culture.
 
   I, @b{Lindsey the Arch Wizard Scientist}, have been studying Kafdor culture for several years and documenting their Runes. I will document new Runes as they come to light.
   }
   #:rune-lores
+
+  ;call it path:ice-storm-demo.mp4 ??
   (list
     (rune-lore
       #:name        "Ice Storm"
       #:rune        (ice-storm-rune)
-      #:description "This rune create a tornado of ice." 
+      #:description @md{
+
+      @(rune-code-example 
+	 (my-mod-lang #:with-paren-runes? #t)
+	 (ice-storm)
+	 ice-storm-demo.mp4
+	)
+
+      }
       )
     (rune-lore
       #:name        "Ice Ball Rune"
       #:rune        (ice-ball-rune)
-      #:description "This rune creates a ball of ice magic." 
-      )
+      #:description 
+      @md{This rune creates a ball of ice magic.
+
+      @(rune-code-example 
+	 (my-mod-lang #:with-paren-runes? #t)
+	 (ice-ball)
+	 ice-ball-demo.mp4)
+      })
     (rune-lore
       #:name        "Ice Ball Hit Rune"
       #:rune        (ice-ball-hit-rune)
-      #:description "This rune create an explosion of ice magic, which lasts only briefly." 
+      #:description @md{
+      This rune create an explosion of ice magic, which lasts only briefly.
+
+      @(rune-code-example 
+	 (my-mod-lang #:with-paren-runes? #t)
+	 (ice-ball-hit)
+	 ice-ball-hit-demo.mp4)
+      }
       )
     (rune-lore
       #:name        "Ice Aura Rune"
       #:rune        (ice-aura-rune)
-      #:description "This rune create an magical aura of ice." 
+      #:description @md{
+      This rune create an magical aura of ice.
+
+      @(rune-code-example 
+	 (my-mod-lang #:with-paren-runes? #t)
+	 (ice-aura)
+	 ice-aura-demo.mp4)
+      }
       )
     )
   #:preview-image preview-image
