34d1689c4a15ab4187b96c51eb248489207dc5ff
Lindsey Handley <lindseyhandley@gmail.com>
2021-02-26 15:22:19 +0000

    release url

--- info.rkt
+++ info.rkt
@@ -5,3 +5,4 @@
 (define pkg-desc "Description Here")
 (define version "0.0")
 (define pkg-authors '(Administrator))
+(define release-url "https://codespells-org.s3.amazonaws.com/ModBuilds/ice-particles/0.0/BuildUnreal.zip") 
